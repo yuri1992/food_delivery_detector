590fc26ac56f6d3cf525415e782221ef5807326f
yurir <yuri1992@gmail.com>
2018-10-13 16:32:57 +0300

    Fix - .SH

--- capture.sh
+++ capture.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+cd /usr/web/food_delivery_detector
 source venv/bin/activate
 python capture.py
--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd /usr/web/food_delivery_detector
 source venv/bin/activate
 export FLASK_APP=server.py
 flask run --host=0.0.0.0
